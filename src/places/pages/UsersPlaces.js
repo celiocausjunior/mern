b285a37d0f17665d51bcce5a2c192c2ec1dce44e
Celio Caus Junior <celiocausjunior@MacBook-Air-de-Celio.local>
2022-03-08 17:12:26 -0300

    Getting route params

--- src/places/pages/UsersPlaces.js
+++ src/places/pages/UsersPlaces.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 import PlacesList from '../components/PlacesList';
 
 const DUMMY_PLACES = [
@@ -6,7 +7,7 @@ const DUMMY_PLACES = [
         id: 'p1',
         title: 'Empire State Building',
         description: 'One of the most famous sky scrapers in the world',
-        imageUrl: 'https://pt.wikipedia.org/wiki/Empire_State_Building#/media/Ficheiro:Empire_State_Building_(aerial_view).jpg',
+        imageUrl: 'https://firebasestorage.googleapis.com/v0/b/mern-ce5b2.appspot.com/o/Empire_State_Building_(aerial_view).jpeg?alt=media&token=2b33313f-5e22-4a40-a583-8ce05682e739',
         address: '20 W 34th St, New York, NY 10001, Estados Unidos',
         location: {
             lat: 40.748817,
@@ -18,7 +19,7 @@ const DUMMY_PLACES = [
         id: 'p2',
         title: 'Empire State Building',
         description: 'One of the most famous sky scrapers in the world',
-        imageUrl: 'https://pt.wikipedia.org/wiki/Empire_State_Building#/media/Ficheiro:Empire_State_Building_(aerial_view).jpg',
+        imageUrl: 'https://firebasestorage.googleapis.com/v0/b/mern-ce5b2.appspot.com/o/Empire_State_Building_(aerial_view).jpeg?alt=media&token=2b33313f-5e22-4a40-a583-8ce05682e739',
         address: '20 W 34th St, New York, NY 10001, Estados Unidos',
         location: {
             lat: 40.748817,
@@ -29,9 +30,9 @@ const DUMMY_PLACES = [
 ];
 
 const UsersPlaces = () => {
-
-    return <PlacesList items={DUMMY_PLACES} />
-
+    const userId = useParams().userId;
+    const loadedPlaces = DUMMY_PLACES.filter((place)=> place.creator === userId);
+    return <PlacesList items={loadedPlaces} />
 };
 
 export default UsersPlaces;
