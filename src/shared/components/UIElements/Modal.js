5539b17462c33ff7c5bd98d0cf6052916b38c3dd
Celio Caus Junior <celiocausjunior@MacBook-Air-de-Celio.local>
2022-03-09 17:16:08 -0300

    Fixing modal

--- src/shared/components/UIElements/Modal.js
+++ src/shared/components/UIElements/Modal.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Backdrop from './Backdrop';
 import { CSSTransition } from 'react-transition-group';
+
+import Backdrop from './Backdrop';
 import './Modal.css';
 
 const ModalOverlay = props => {
     const content = (
         <div className={`modal ${props.className}`} style={props.style}>
-            <header className={`modal-header ${props.headerClass}`}>
+            <header className={`modal__header ${props.headerClass}`}>
                 <h2>{props.header}</h2>
             </header>
-            <form onSubmit={props.onSubmit ? props.onSubmit : event => event.preventDefault()}>
-                <div className={`modal_content ${props.contentClass}`}>
+            <form
+                onSubmit={
+                    props.onSubmit ? props.onSubmit : event => event.preventDefault()
+                }
+            >
+                <div className={`modal__content ${props.contentClass}`}>
                     {props.children}
                 </div>
-                <footer className={`modal_footer ${props.footerClass}`}>
+                <footer className={`modal__footer ${props.footerClass}`}>
                     {props.footer}
                 </footer>
             </form>
@@ -25,12 +30,18 @@ const ModalOverlay = props => {
 
 const Modal = props => {
     return (
-        <>
+        <React.Fragment>
             {props.show && <Backdrop onClick={props.onCancel} />}
-            <CSSTransition in={props.show} mountOnEnter unmountOnExit timeout={200} classNames='modal'>
+            <CSSTransition
+                in={props.show}
+                mountOnEnter
+                unmountOnExit
+                timeout={200}
+                classNames="modal"
+            >
                 <ModalOverlay {...props} />
             </CSSTransition>
-        </>
+        </React.Fragment>
     );
 };
 
